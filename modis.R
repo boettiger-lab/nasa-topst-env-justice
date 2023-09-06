b77d57ca9ee247642e91cecf56e9fbcd06ec4a4d
Carl <cboettig@gmail.com>
2023-09-06 03:37:05 +0000

    fixed

--- modis.R
+++ modis.R
@@ -1,7 +1,7 @@
 library(rstac)
 library(gdalcubes)
 source('https://gist.github.com/cboettig/5401bd149a2a27bde2042aa4f7cde25b/raw/d360b381f6e16ae518532ef1777116f175c471b5/ed_set_token.R')
-header <- ed_set_token()
+header <- edl_set_token()
 gdalcubes_set_gdal_config("GDAL_HTTP_HEADERS", header)
 
 gdalcubes_options(parallel = TRUE) 
@@ -11,7 +11,8 @@ library(sf)
 CA <- spData::us_states |> dplyr::filter(NAME=="California")
 bbox <- CA |> st_bbox()
 
-box = CA |> vect() |> ext()
+#library(terra)
+#box = CA |> vect() |> ext()
 
 start <- "2022-07-01"
 end <- "2022-07-31"
@@ -24,31 +25,13 @@ items <- stac("https://cmr.earthdata.nasa.gov/stac/LPCLOUD") |>
   items_fetch()
 
 
-library(httr)
-localize <- function(x) {
-  dest <- basename(x)
- bearer <- gsub("Authorization: ", "", header)
-  httr::GET(href, 
-            httr::write_disk(dest, overwrite = TRUE),
-            httr::add_headers(Authorization= bearer)
-            #httr::authenticate(Sys.getenv("EARTHDATA_USER"),
-            #                   Sys.getenv("EARTHDATA_PASSWORD"))
-            )
-  dest
-}
-
-## proof-of-concept
-href <- items$features[[15]]$assets$data$href
-x <- terra::rast(localize(href))
-#library(stars)
-#read_stars(paths[[15]]) |> plot()
-rast(paths, 1) |> terra::project("EPSG:4326") |> crop(box) |>  plot()
+paths <- edl_stac_urls(items) |> purrr::map_chr(edl_download)
 
 
-paths <- purrr::map_chr(items$features, list("assets", "data", "href")) |>
-  purrr::map_chr(localize)
 col <- gdalcubes::create_image_collection(paths, format = "MxD13Q1")
-# col <- stac_image_collection(items$features, asset_names = "data",  url_fun = localize)
+#col <- stac_image_collection(items$features, 
+#                             asset_names = "data", 
+#                             url_fun = edl_download)
 
 v = cube_view(srs = "EPSG:4326",
               extent = list(t0 = as.character(start), 
@@ -57,11 +40,9 @@ v = cube_view(srs = "EPSG:4326",
                             top = bbox[4], bottom = bbox[2]),
               nx = 512, ny = 512, dt = "P1M")
 
-raster_cube(col, v)|> plot()
+raster_cube(col, v)|> select_bands("NDVI") |> plot()
 
-r <- raster_cube(col, v) |> select_bands("NDVI") |> st_as_stars()
+r <- raster_cube(col, v) |> select_bands("NDVI") |> st_as_stars.cube()
 library(tmap)
-tm_shape(CA) + tm_polygons() + tm_shape(r) + tm_raster("NDVI")
+tm_shape(r) + tm_raster("NDVI") + tm_shape(CA) + tm_borders()
 
-library(terra)
-terra::rast("test/cube_1ac2ea680dc2018-06-01.tif") 
