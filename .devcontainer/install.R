b1902cbf2b67c06db1f57328c29130125b90c8e2
Carl Boettiger <cboettig@gmail.com>
2023-09-28 02:17:55 +0000

    notes

--- .devcontainer/install.R
+++ .devcontainer/install.R
@@ -1,3 +1,8 @@
+# install R dependencies
+
+# We could use renv.lock approach here instead, but will force re-creation of environment from scratch
+# Does not provide a good way to ensure that sf/terra/gdalcubes are installed from source while other packages can be binary
+
 install.packages("rstac", "spData")
 remotes::install_github("boettiger-lab/earthdatalogin")
 remotes::install_github('r-tmap/tmap')
