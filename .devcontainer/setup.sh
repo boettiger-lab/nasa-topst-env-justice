142ed20ed062aec636a43b6b846019b6d0ede67b
Carl Boettiger <cboettig@gmail.com>
2023-11-05 15:50:37 -0800

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## permission fails
+#sudo echo "auth-none=1" >> /etc/rstudio/rserver.conf
+
+
+## set startup dir
+mkdir -p ~/.local/share/rstudio/projects_settings
+echo "/workspaces/nasa-topst-env-justice/nasa-topst-env-justice.Rproj" > ~/.local/share/rstudio/projects_settings/last-project-path
+
