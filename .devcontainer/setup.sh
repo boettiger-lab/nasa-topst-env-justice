1a86f89cfafca89b25591811847ad9d28175de0b
Carl Boettiger <cboettig@gmail.com>
2023-11-06 11:19:27 -0800

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,7 +5,7 @@ sudo sudo bash -c 'echo "USER=rstudio" >>/etc/environment'
 sudo /init &
 
 ## set startup dir
-# mkdir -p ~/.local/share/rstudio/projects_settings
-# echo "/workspaces/nasa-topst-env-justice/nasa-topst-env-justice.Rproj" > ~/.local/share/rstudio/projects_settings/last-project-path
+mkdir -p ~/.local/share/rstudio/projects_settings
+echo "/workspaces/nasa-topst-env-justice/nasa-topst-env-justice.Rproj" > ~/.local/share/rstudio/projects_settings/last-project-path
 
-echo "setup complete\n"
+echo "setup complete"
