fe393089b06d73c6a19d0d0827d9c2e8696b3a55
Carl Boettiger <cboettig@gmail.com>
2023-11-22 00:38:58 +0000

    devcontainer testing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,7 @@ sudo /init &
 
 ## set startup dir
 mkdir -p ~/.local/share/rstudio/projects_settings
-echo "/workspaces/nasa-topst-env-justice/nasa-topst-env-justice.Rproj" > ~/.local/share/rstudio/projects_settings/last-project-path
+export RPROJ"=$(ls ${CODESPACE_VSCODE_FOLDER}/*.Rproj)"
+echo ${RPROJ} > ~/.local/share/rstudio/projects_settings/last-project-path
 
-echo "setup complete"
+
