2d62d88da547417af6f59f52c0af8f9ed22eedb4
Carl <cboettig@gmail.com>
2023-11-27 11:32:24 -0800

    gitpod as gitpod

--- .gitpod-setup.sh
+++ .gitpod-setup.sh
@@ -1,7 +1,5 @@
-sudo useradd rstudio -u 1000 -s /bin/bash
+export DEFAULT_USER=gitpod
 mkdir -p ~/.local/share/rstudio/projects_settings
-sudo mkdir -p /home/rstudio/.local/share/rstudio/projects_settings
 export RPROJ"=$(ls -d $PWD/*.Rproj)"
-sudo echo ${RPROJ} > ~/.local/share/rstudio/projects_settings/last-project-path
-sudo cp ~/.local/share/rstudio/projects_settings/last-project-path /home/rstudio/.local/share/rstudio/projects_settings/last-project-path
-sudo chown -R rstudio:rstudio /home/rstudio/.local/share/rstudio
+echo ${RPROJ} > ~/.local/share/rstudio/projects_settings/last-project-path
+
